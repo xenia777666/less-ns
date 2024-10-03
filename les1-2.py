eb5b66d4991b9dbd532a8376699305f8bbc2ae7a
jsxenix <143598349+xenia777666@users.noreply.github.com>
2024-10-03 20:23:30 +0300

    less2

--- les1-2.py
+++ les1-2.py
@@ -22,7 +22,6 @@ fake_trades = [
     {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
 ]
 
-
 @app.get("/trades")
 def get_trades(limit: int = 1, offset: int = 0):
     return fake_trades[offset:][:limit]
@@ -34,12 +33,34 @@ fake_users2 = [
     {"id": 3, "role": "trader", "name": "Matt"},
 ]
 
-
 @app.post("/users/{user_id}")
 def change_user_name(user_id: int, new_name: str):
     current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
     current_user["name"] = new_name
     return {"status": 200, "data": current_user}
+
+
+
+#lesson3
+fake_trades = [
+    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
+    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
+]
+
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5) #валидация данных при вводе от пользователя с помощью pydantic
+    side: str
+    price: float = Field(ge=0)
+    amount: float
+
+
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
 
 
 
